5093dc73df4d3350428b0ed558d591ad337b1efd
Alan-Kuan <kuanhaoan@gmail.com>
2022-12-01 22:01:01 +0800

    chore: move files & add Makefile

---
+++ Makefile
@@ -0,0 +1,18 @@
+CC=gcc
+CCFLAGS=-I include -O3 -Wall
+OBJS=objs/arg_handler.o objs/list.o objs/main.o objs/ringbuf.o objs/worker.o
+EXE=pp-find
+
+.PHONY: all clean
+
+all: $(EXE)
+
+$(EXE): $(OBJS)
+	$(CC) -o $@ $^
+
+objs/%.o: src/%.c
+	$(CC) -o $@ -c $^ $(CCFLAGS)
+
+clean:
+	-rm $(EXE)
+	-rm $(OBJS)
--- include/ringbuf.h
+++ include/ringbuf.h
@@ -1,3 +1,19 @@
 #ifndef RINGBUF_H_
 #define RINGBUF_H_
+
+#include <stdint.h>
+#include <stdatomic.h>
+
+struct ringbuf {
+    _Atomic(uint64_t) head;
+    _Atomic(uint64_t) tail;
+    uint64_t capacity;
+    char** buf;
+};
+
+void init_ringbuf(struct ringbuf** ringbuf);
+void destroy_ringbuf(struct ringbuf* ringbuf);
+void push_ringbuf(struct ringbuf* ringbuf, char* item);
+char* pop_ringbuf(struct ringbuf* ringbuf);
+
 #endif
