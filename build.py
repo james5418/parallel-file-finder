456d95139bbf6cf86b096c6a4ba85ccb3b496115
Alan-Kuan <kuanhaoan@gmail.com>
2022-12-10 13:48:42 +0800

    feat: adjust soem option's default value of build.py

--- build.py
+++ build.py
@@ -35,16 +35,19 @@ def build(curr_path, curr_d, n, m, d, f):
         build(dir_path, next_d, n, m, d, f)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser()
     parser.add_argument('-n', nargs='?', type=int, default=2,
-                        help='number of directories per level')
-    parser.add_argument('-m', nargs='?', type=int, default=2,
-                        help='number of directories at each level that can grow subdirectories')
+                        help='number of directories per level (default: 2)')
+    parser.add_argument('-m', nargs='?', type=int,
+                        help='number of directories at each level that can grow subdirectories (default: N)')
     parser.add_argument('-d', nargs='?', type=int, default=3,
-                        help='depth of this structure')
+                        help='depth of this structure (default: 3)')
     parser.add_argument('-f', nargs='?', type=int, default=5,
-                        help='number of files per level')
+                        help='number of files per level (default: 5)')
     args = parser.parse_args()
+
+    if args.m is None:
+        args.m = args.n
 
     struct_type = 'balanced' if args.n == args.m else 'imbalanced'
     base_root = f'{ struct_type }_{ args.n }_{ args.m }_{ args.d }_{ args.f }'
