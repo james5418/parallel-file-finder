2dcfe8d8f4d22cec126b625b6d681b350394676f
Alan-Kuan <kuanhaoan@gmail.com>
2022-12-11 18:48:36 +0800

    test: add option to determine the order of directories and files

--- test/tree.py
+++ test/tree.py
@@ -3,11 +3,12 @@ import os
 import argparse
 from utils.utils import *
 
-def build(curr_path, curr_d, n, m, d, f):
-    # create files
-    for i in range(f):
-        file_path = f'{ curr_path }/f_{ get_rand_name() }'
-        touch(file_path)
+def build(curr_path, curr_d, n, m, d, f, b):
+    # create files before subdirs
+    if b or curr_d == d:
+        for i in range(f):
+            file_path = f'{ curr_path }/f_{ get_rand_name() }'
+            touch(file_path)
 
     if curr_d == d:
         return
@@ -17,7 +18,13 @@ def build(curr_path, curr_d, n, m, d, f):
         dir_path = f'{ curr_path }/d_{ get_rand_name() }'
         os.mkdir(dir_path)
         next_d = curr_d + 1 if i < m else d
-        build(dir_path, next_d, n, m, d, f)
+        build(dir_path, next_d, n, m, d, f, b)
+
+    # create files after subdirs
+    if not b:
+        for i in range(f):
+            file_path = f'{ curr_path }/f_{ get_rand_name() }'
+            touch(file_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -29,6 +36,8 @@ if __name__ == '__main__':
                         help='depth of this structure (default: 3)')
     parser.add_argument('-f', nargs='?', type=int, default=5,
                         help='number of files per level (default: 5)')
+    parser.add_argument('-b', action='store_true',
+                        help='whether to put every directory behind files')
     args = parser.parse_args()
 
     if args.m is None:
@@ -36,9 +45,11 @@ if __name__ == '__main__':
 
     struct_type = 'balanced' if args.n == args.m else 'imbalanced'
     base_root = f'{ struct_type }_{ args.n }_{ args.m }_{ args.d }_{ args.f }'
+    if args.b:
+        base_root += '-behind'
 
     root = get_unique_name(base_root)
     os.mkdir(root)
 
-    build(root, 0, args.n, args.m, args.d, args.f)
+    build(root, 0, args.n, args.m, args.d, args.f, args.b)
     print(f'Directory structure "{ root }" was created.')
