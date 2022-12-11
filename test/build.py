8d53a6401eb8df76838cae7a6b84f20a644f3682
Alan-Kuan <kuanhaoan@gmail.com>
2022-12-11 22:21:47 +0800

    fix: build.py

--- test/build.py
+++ test/build.py
@@ -4,20 +4,28 @@ import argparse
 from utils.utils import *
 
 def build(curr_path, curr_d, n, m, d, l, f, b):
+    no_subdir = False
+    if curr_d < 0:
+        curr_d *= -1
+        no_subdir = True
+    is_leaf = curr_d == d or no_subdir
+
     # create files before subdirs
-    if (b or curr_d == d) and (curr_d >= l):
+    if (b or is_leaf) and (curr_d >= l):
         for i in range(f):
             file_path = f'{ curr_path }/f_{ get_rand_name() }'
             touch(file_path)
 
-    if curr_d == d:
+    if is_leaf:
         return
 
     # create subdirs
     for i in range(n):
         dir_path = f'{ curr_path }/d_{ get_rand_name() }'
         os.mkdir(dir_path)
-        next_d = curr_d + 1 if i < m else d
+        next_d = curr_d + 1
+        if i >= m:
+            next_d *= -1
         build(dir_path, next_d, n, m, d, l, f, b)
 
     # create files after subdirs
